2108aa0c4197713cd1bcde8538d01fd2cfcd4cae
Evgeny Ankudinov <ankudinoff@gmail.com>
2014-09-10 00:50:36 +0400

    Переписал через forEach метод savePosts в Storage,

--- Storage.js
+++ Storage.js
@@ -13,26 +13,28 @@ var Storage = function (connectionString) {
     });
 };
 
-Storage.prototype.countOfPosts = function (owner_id) {
-
+Storage.prototype.countOfPosts = function (owner_id, callback) {
 };
 
-Storage.prototype.savePosts = function (posts) {
-    for (var post in posts) {
-        var insertPost = {
+Storage.prototype.savePosts = function (posts, callback) {
+    var self = this;
+    posts.forEach(function (post, index) {
+        var sql = {
             text: 'INSERT INTO posts(post_id, owner_id, from_id, ' +
                 'create_time, likes, reposts, jsondata) ' +
                 'VALUES ($1,$2,$3,to_timestamp($4),$5,$6,$7)',
             values: [ post.id, post.owner_id, post.from_id,
-                post.date, post.likes, post.reposts,
+                post.date, post.likes.count, post.reposts.count,
                     '' + JSON.stringify(post) + '']
         };
-        this.client.query(insertPost, function (err, res) {
+        self.client.query(sql, function (err, res) {
             if (err) {
-                throw err;
+                console.error(err);
             }
+            if (index === posts.length - 1)
+                callback();
         });
-    }
+    });
 };
 
 Storage.prototype.end = function () {
